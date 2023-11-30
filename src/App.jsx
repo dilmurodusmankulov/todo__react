64f8ea3e5f7be3f3e63ed0742b66ad69f2b5ace9
dilmurodusmankulov <dilmurodusmankulov777@gmail.com>
2023-12-01 00:18:31 +0500

    react__todo

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { Form } from "./components/Form";
+import { Todos } from "./components/Todos";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteAll } from "./redux/todoapp/actions";
+
+function App() {
+  const dispatch = useDispatch();
+
+  const todos = useSelector((state) => state.operationsReducer);
+
+  const [editFormVisibility, setEditFormVisibility] = useState(false);
+
+  const [editTodo, setEditTodo] = useState("");
+
+  const handleEditClick = (todo) => {
+    setEditFormVisibility(true);
+    setEditTodo(todo);
+  };
+
+  const cancelUpdate = () => {
+    setEditFormVisibility(false);
+  };
+
+  return (
+    <div className="wrapper">
+      <br></br>
+      <h1 className="text-center">Dilmurod__Programmer Todo Redux N19</h1>
+      <Form
+        editFormVisibility={editFormVisibility}
+        editTodo={editTodo}
+        cancelUpdate={cancelUpdate}
+      />
+      <Todos
+        handleEditClick={handleEditClick}
+        editFormVisibility={editFormVisibility}
+      />
+      {todos.length > 1 && (
+        <button
+          className="btn btn-danger btn-md delete-all"
+          onClick={() => dispatch(deleteAll())}
+        >
+          Hammasini o'chirish
+        </button>
+      )}
+      <marquee behavior="" direction="">
+        <h1>Dilmurod__Programmer</h1>
+      </marquee>
+    </div>
+  );
+}
+
+export default App;
